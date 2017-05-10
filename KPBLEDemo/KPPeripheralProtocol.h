fe1b06d133c86148918f061c905972a374e9aed4
CPX <chenpeixiang@CPXdeMacBook-Pro.local>
2017-05-10 18:36:19 +0800

    not first commit

---
+++ KPBLEDemo/KPPeripheralProtocol.h
@@ -0,0 +1,13 @@
+//
+//  KPPeripheralProtocol.h
+//  KPBLEDemo
+//
+//  Created by CPX on 10/05/2017.
+//  Copyright © 2017 KaoPuJinFu. All rights reserved.
+//
+
+#ifndef KPPeripheralProtocol_h
+#define KPPeripheralProtocol_h
+
+
+#endif /* KPPeripheralProtocol_h */
